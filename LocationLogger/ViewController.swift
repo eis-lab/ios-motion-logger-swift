2223d39a4f66127f866a780d696ac24882079792
Jaewon Choi <jaewon.james.choi@gmail.com>
2023-03-03 09:43:04 +0900

    Initial commit

--- LocationLogger/ViewController.swift
+++ LocationLogger/ViewController.swift
@@ -6,14 +6,78 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreMotion
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    let motionManager: CMMotionManager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let locationManager = CLLocationManager()
+        locationManager.delegate = self
+
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
+        motionManager.stopAccelerometerUpdates()
+        motionManager.stopGyroUpdates()
+        motionManager.stopMagnetometerUpdates()
+
+        if motionManager.isAccelerometerAvailable {
+            print("Accelerometer is available")
+            motionManager.accelerometerUpdateInterval = 0.01
+            motionManager.startAccelerometerUpdates(to: OperationQueue.main) { (data, error) in
+                guard let acceleraometerData = data else { return }
+                print("Accelerometer: \(acceleraometerData)")
+            }
+        }
+
+        if motionManager.isGyroAvailable {
+            print("Gyro is available")
+            motionManager.gyroUpdateInterval = 0.01
+            motionManager.startGyroUpdates(to: OperationQueue.main) { (data, error) in
+                guard let gyroData = data else { return }
+                print("Gyro: \(gyroData)")
+            }
+        }
+
+        if motionManager.isMagnetometerAvailable {
+            print("Magnetometer is available")
+            motionManager.magnetometerUpdateInterval = 0.01
+            motionManager.startMagnetometerUpdates(to: OperationQueue.main) { (data, error) in
+                guard let magnetometerData = data else { return }
+                print("Mag: \(magnetometerData)")
+            }
+        }
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for location in locations {
+            print(location)
+        }
+    }
 
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print(region)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print(region)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailRangingFor beaconConstraint: CLBeaconIdentityConstraint, error: Error) {
+        print(error)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
